1eccdf676065345cf1811271a8cf0f4abe780191
Cedric Ziel <cedric.ziel@instana.com>
2022-02-28 10:16:56 +0100

    Transform MySQL service to StatefulSet (#48)

---
+++ mysql/docker-entrypoint-initdb.d/20-ratings.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ratings DEFAULT CHARACTER SET 'utf8';
+
+USE ratings;
+
+CREATE TABLE ratings (
+    sku varchar(80) NOT NULL,
+    avg_rating DECIMAL(3, 2) NOT NULL,
+    rating_count INT NOT NULL,
+    PRIMARY KEY (sku)
+) ENGINE=InnoDB;
+
+GRANT ALL ON ratings.* TO 'ratings'@'%' IDENTIFIED BY 'iloveit';
+GRANT ALL ON ratings.* TO 'ratings'@'localhost' IDENTIFIED BY 'iloveit';
