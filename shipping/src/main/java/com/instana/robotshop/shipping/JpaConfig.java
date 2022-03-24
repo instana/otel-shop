2f72d7a9803eb7abee41819544b5ae84649bf3ee
Cedric Ziel <cedric.ziel@instana.com>
2022-03-24 12:00:16 +0100

    Update to MySQL 8: allowPublicKeyRetrieval=True

--- shipping/src/main/java/com/instana/robotshop/shipping/JpaConfig.java
+++ shipping/src/main/java/com/instana/robotshop/shipping/JpaConfig.java
@@ -13,7 +13,7 @@ public class JpaConfig {
 
     @Bean
     public DataSource getDataSource() {
-        String JDBC_URL = String.format("jdbc:mysql://%s/cities?useSSL=false&autoReconnect=true", System.getenv("DB_HOST") == null ? "mysql" : System.getenv("DB_HOST"));
+        String JDBC_URL = String.format("jdbc:mysql://%s/cities?useSSL=false&autoReconnect=true&allowPublicKeyRetrieval=True", System.getenv("DB_HOST") == null ? "mysql" : System.getenv("DB_HOST"));
 
         logger.info("jdbc url {}", JDBC_URL);
 
