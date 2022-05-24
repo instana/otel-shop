00de3c3edeec938c8ce4802f1d56037f96732013
esara <endre.sara@turbonomic.com>
2022-05-24 11:14:27 -0400

    turn off deprecation errors in opentelemetry-php while in alpha status
    Signed-off-by: esara <endre.sara@turbonomic.com>

--- ratings/html/index.php
+++ ratings/html/index.php
@@ -7,6 +7,9 @@ require __DIR__.'/vendor/autoload.php';
 use Instana\RobotShop\Ratings\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
+# Turn off deprecation messages as per https://github.com/open-telemetry/opentelemetry-php/commit/99fba0d720785dfe9faba310e6e2e78fae9c075d
+OpenTelemetry\SDK\Common\Dev\Compatibility\Util::setErrorLevel(0)
+
 $env = getenv('APP_ENV') ?: 'dev';
 $kernel = new Kernel($env, true);
 $request = Request::createFromGlobals();
